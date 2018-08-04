d317e5e70e8dd005683ef7a7b8d0f45336e827e5
Bach Le <bach@bullno1.com>
2018-08-05 04:42:06 +0800

    Add test for creating order

--- test/acmerl_SUITE.erl
+++ test/acmerl_SUITE.erl
@@ -8,12 +8,15 @@
 
 all() -> [ new_client
          , {group, with_client}
+         , {group, with_account}
          ].
 
 groups() ->
     [ {with_client, [shuffle], [ create_account_from_new_key
                                , create_account_from_existing_key
                                ]}
+    , {with_account, [shuffle], [ create_order
+                                ]}
     ].
 
 init_per_suite(Config) ->
@@ -26,8 +29,9 @@ end_per_suite(Config) ->
     ok.
 
 init_per_group(with_client, Config) ->
-    {ok, Client} = acmerl:new_client(?DIRECTORY_URL, client_opts()),
-    [{client, Client} | Config].
+    create_client(Config);
+init_per_group(with_account, Config) ->
+    create_account(create_client(Config)).
 
 end_per_group(_, _) -> ok.
 
@@ -66,6 +70,24 @@ create_account_from_existing_key(Config) ->
     ),
     ok.
 
+create_order(Config) ->
+    Client = proplists:get_value(client, Config),
+    Account = proplists:get_value(account, Config),
+
+    Identifiers = [ #{ <<"type">> => <<"dns">>
+                     , <<"value">> => <<"example.com">>
+                     }
+                  ,  #{ <<"type">> => <<"dns">>
+                      , <<"value">> => <<"www.example.com">>
+                      }
+                  ],
+    OrderOpts = #{<<"identifiers">> => Identifiers},
+    {ok, Order} = acmerl:new_order(Client, Account, OrderOpts),
+    {ok, Authorizations} = acmerl:order_authorizations(Client, Order),
+    ?assertEqual(length(Identifiers), length(Authorizations)),
+
+    ok.
+
 % Helpers
 
 start_apps(Apps) ->
@@ -82,6 +104,16 @@ start_apps(Apps) ->
       {ok, []},
       Apps
      ).
+
+create_client(Config) ->
+    {ok, Client} = acmerl:new_client(?DIRECTORY_URL, client_opts()),
+    [{client, Client} | Config].
+
+create_account(Config) ->
+    Client = proplists:get_value(client, Config),
+    AccountOpts = #{<<"termsOfServiceAgreed">> => true},
+    {ok, Account} = acmerl:new_account(Client, AccountOpts),
+    [{account, Account} | Config].
 
 client_opts() ->
     #{ http_client => fun http_client/4
